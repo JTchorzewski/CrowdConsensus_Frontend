a1d04ef84ca8631daa98a5de04260a9e9372d77b
Bazyli Niesłuchowski <niesluchowskibazyli@gmail.com>
2025-05-17 00:31:32 +0200

    privateroute

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import LandingPage  from './pages/LandingPage';
 import LoginPage    from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import MainPage     from './pages/MainPage';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
@@ -11,7 +12,14 @@ function App() {
       <Route path="/"         element={<LandingPage />}  />
       <Route path="/login"    element={<LoginPage />}    />
       <Route path="/register" element={<RegisterPage />} />
-      <Route path="/spolki"   element={<MainPage />}      />
+        <Route
+        path="/spolki"
+        element={
+          <PrivateRoute>
+            <MainPage />
+          </PrivateRoute>
+        }
+      />
     </Routes>
   );
 }
